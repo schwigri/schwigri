1ee0dd976e61ca4679763cd51af158d14a6e1d06
Griffen Schwiesow <schwigri@pm.me>
2021-02-16 22:48:55 -0800

    feat: setup gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	siteMetadata: {
+		title: "Griffen Schwiesow",
+	},
+	plugins: [],
+};
---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Error404Page extends React.Component {
+	render() {
+		return <p>Error 404 page</p>;
+	}
+}
+
+export default Error404Page;
